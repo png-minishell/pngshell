b820fee1483ce5053e6a76527b49f04ff65d753a
June <khsj15@khu.ac.kr>
2022-08-19 19:51:09 +0900

    add error_functions

--- include/error_control_functions.h
+++ include/error_control_functions.h
@@ -6,15 +6,21 @@
 /*   By: sungjpar <sungjpar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 14:53:07 by sungjpar          #+#    #+#             */
-/*   Updated: 2022/08/17 14:53:08 by sungjpar         ###   ########.fr       */
+/*   Updated: 2022/08/19 19:50:29 by sungjpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERROR_CONTROL_FUNCTIONS_H
 # define ERROR_CONTROL_FUNCTIONS_H
 
+# include <unistd.h>
 # include <stddef.h>
+# include "minishell_definitions.h"
 
-void	*e_malloc(size_t malloc_size);
+void		*e_malloc(size_t malloc_size);
+t_status	e_close(const int fd);
+t_status	e_dup2(const int fd1, const int fd2);
+pid_t		e_fork(void);
+int			e_open(const char *path, int flag, int mode);
 
 #endif
